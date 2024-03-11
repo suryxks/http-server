a4772685f787a837496945fa6062de53fb8e34e6
suryxks <suryasuresh9876543@gmail.com>
2024-03-11 09:01:25 +0530

    respond with content

--- app/server.go
+++ app/server.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
 const (
-	OK        = "HTTP/1.1 200 OK\r\n\r\n"
-	NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n"
+	OK                    = "HTTP/1.1 200 OK\r\n\r\n"
+	NOT_FOUND             = "HTTP/1.1 404 Not Found\r\n\r\n"
+	CONTENT_TYPE_TEXT     = "Content-Type: text/plain"
+	CONTENT_LENGTH_HEADER = "Content-Length: "
+	USER_AGENT_HEADER     = "User-Agent: "
 )
 
 func main() {
@@ -38,7 +42,10 @@ func main() {
 
 	header := strings.Split(string(data), "\r\n")[0]
 	path := strings.Split(header, " ")[1]
-	if path == "/" {
+	if strings.Contains(path, "/echo") {
+		_, content, _ := strings.Cut(path, "/echo/")
+		connection.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(content)) + "\r\n\r\n" + content))
+	} else if path == "/" {
 		connection.Write([]byte(OK))
 	} else {
 		connection.Write([]byte(NOT_FOUND))
